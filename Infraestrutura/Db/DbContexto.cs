3dcea61e85f4e2523d1610411cd3bc1d60882cee
RafaFontoura <rafaelcosta12597@gmail.com>
2025-09-06 16:45:02 -0300

    Atualiza projeto com novas alterações

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,35 @@
-﻿namespace Minimal_API.Infraestrutura.Db
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_API.Dominio.Entidades;
+
+namespace Minimal_API.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
+        private readonly IConfiguration _configuracaoAppSettings;
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        { 
+            _configuracaoAppSettings = configuracaoAppSettings;
+
+        }
+
+        public DbSet<Administrador> administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            if(optionsBuilder.IsConfigured)
+
+            { 
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql");
+            if (!string.IsNullOrEmpty(stringConexao)) 
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Minimal_API.Infraestrutura.Db;
+
+using MinimalAPI.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+            builder.Configuration.GetConnectionString("mysql"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+
+     );
+});
+
+
 
 var app = builder.Build();
 
-app.MapPost("/login", (MinimalAPI.DTOs.LoginDTO loginDTO) => {
-if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
-    return Results.Ok("Login com sucesso");
-else
-    return Results.Unauthorized();
+app.MapPost("/login", (MinimalAPI.DTOs.LoginDTO loginDTO) =>
 {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+    {
 
+    }
 });
 
 app.Run();
